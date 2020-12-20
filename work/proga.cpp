5dc96da099fc4cad5d0f48ed72b4caea3302bfee
JUTEEXs <a.slawik2002@yandex.ru>
2020-12-20 19:56:26 +0300

    First commit

---
+++ work/proga.cpp
@@ -0,0 +1,23 @@
+﻿// proga.cpp
+
+#include <iostream>
+using namespace std;
+const int i = 2;
+int mas[i];
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	mas[0] = 2;
+	mas[1] = 5;
+	mas[2] = -4;
+	int z = 0;
+	for (int i = 0; i < 2; i++)
+	{
+		if (mas[i] > 0)
+			z = z + 1;
+	}
+	cout << "количество положительных числел ="<< z;
+	cout << "Change";
+	return 0;
+}
